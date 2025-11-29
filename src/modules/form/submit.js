2192220b7bc552bd540a3ed74cb87a94d9ef59fa
NathaMars <nathanmarques.silva@gmail.com>
2025-11-29 17:33:43 -0300

    Fix: A listagem de agendamentos é atualizada automaticamente

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs"
 
 import { scheduleNew } from "../../services/schedule-new.js"
+import { schedulesDay} from "../schedules/load.js"
 
 const form = document.querySelector("form")
 const selectDate = document.getElementById("date")
@@ -27,6 +28,10 @@ form.onsubmit = async (event) => {
         const id = new Date().getTime()
 
        await scheduleNew({ id, name, when})
+
+       await schedulesDay()
+
+       clientName.value = ""
 
     } catch (error) {
         alert("Ocorreu um erro ao enviar o formulário.")
